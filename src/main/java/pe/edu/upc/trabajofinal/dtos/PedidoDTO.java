369468c7501f09fe73ffb7d1956ec7009993acb2
MIRIAN <miriancontreraspaquita@gmail.com>
2024-09-10 15:19:12 -0500

    se  agrego relaciones

--- src/main/java/pe/edu/upc/trabajofinal/dtos/PedidoDTO.java
+++ src/main/java/pe/edu/upc/trabajofinal/dtos/PedidoDTO.java
@@ -1,21 +1,27 @@
 package pe.edu.upc.trabajofinal.dtos;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import pe.edu.upc.trabajofinal.Entities.Carrito;
+import pe.edu.upc.trabajofinal.Entities.Productos;
 
 import java.time.LocalDate;
 
 public class PedidoDTO {
-    private int id;
+    private int idPedido ;
     private LocalDate fechacPedido;
     private LocalDate fechaEntrega;
     private String estado;
+    private Carrito c;
+    private Productos p;
 
-    public int getId() {
-        return id;
+    public int getIdPedido() {
+        return idPedido;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
     }
 
     public LocalDate getFechacPedido() {
@@ -40,5 +46,21 @@ public class PedidoDTO {
 
     public void setEstado(String estado) {
         this.estado = estado;
+    }
+
+    public Carrito getC() {
+        return c;
+    }
+
+    public void setC(Carrito c) {
+        this.c = c;
+    }
+
+    public Productos getP() {
+        return p;
+    }
+
+    public void setP(Productos p) {
+        this.p = p;
     }
 }
